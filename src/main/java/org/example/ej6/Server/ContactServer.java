6d4dbd4f4a5e642fe4de9825ff2c3fd1233ae526
a22angelgp <a22angelgp@iessanclemente.net>
2024-01-31 12:33:22 +0100

    minorities uwuntu 2

--- src/main/java/org/example/ej6/Server/ContactServer.java
+++ src/main/java/org/example/ej6/Server/ContactServer.java
@@ -1,4 +1,5 @@
 package org.example.ej6.Server;
 
 public class ContactServer {
+    //TODO: implement this
 }
--- src/main/java/org/example/ej6/Server/ContactServerWorker.java
+++ src/main/java/org/example/ej6/Server/ContactServerWorker.java
@@ -1,4 +1,5 @@
 package org.example.ej6.Server;
 
 public class ContactServerWorker {
+    //TODO: implement this
 }
